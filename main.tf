1f442e3d7265f6d13e8f2d6fdf147e0f18fb17b4
Kavana1997 <102670832+Kavana1997@users.noreply.github.com>
2024-10-18 16:02:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "kaah-backend"
+    region = "ap-south-1"
+    key = "backend/terraform.tfstate"
+    dynamodb_table = "backend-dynamoDB"
+    
+  }
+}
+
 resource "aws_instance" "instance-tag1"{
   ami = "ami-08718895af4dfa033"
   instance_type = "t2.micro"
   key_name = "new_jenkins"
   tags = {
-    Name = "terraform-instance1"
+    Name = "atlantis-terraform"
   }
 } 
